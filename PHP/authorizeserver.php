ec4f24a9026fd278b92a95cbc1f48496aaf5f385
anuabarnab4 <anuabarna.sf4019@gmail.com>
2024-12-14 14:37:35 +0530

    resolved issue when checking with onpremise site

--- PHP/authorizeserver.php
+++ PHP/authorizeserver.php
@@ -56,6 +56,8 @@ function GetEmbedDetails($embedQuerString, $dashboardServerApiUrl){
       "Content-Type: application/json"
     ),
   ));
+  curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
   $response = curl_exec($curl);
   $err = curl_error($curl);
   curl_close($curl);
